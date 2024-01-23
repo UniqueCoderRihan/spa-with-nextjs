eb33a7f051d45038e278c6a9b00024d7f410d4ee
UniqueCoderrihan <rihanthedev@gmail.com>
2024-01-23 15:03:23 +0600

    data added

---
+++ public/data.js
@@ -0,0 +1,50 @@
+const coures = [
+    {
+        couresName: "C Programming",
+        id:1,
+        img: "https://i.postimg.cc/fTZdzpQ3/Rectangle-2-2.png",
+        price: 450,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+    {
+        couresName: "Learn Algorithom",
+        id:2,
+        img: "https://i.postimg.cc/ry9mJp7m/Rectangle-2-3.png",
+        price: 450,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+    {
+        couresName: "Learn Data Stucture",
+        id:3,
+        img: "https://i.postimg.cc/ry9mJp7m/Rectangle-2-3.png",
+        price: 4250,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+    {
+        couresName: "Learn Data Stucture",
+        id:4,
+        img: "https://i.postimg.cc/ry9mJp7m/Rectangle-2-3.png",
+        price: 4250,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+    {
+        couresName: "Learn Data Stucture",
+        id:5,
+        img: "https://i.postimg.cc/ry9mJp7m/Rectangle-2-3.png",
+        price: 4250,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+    {
+        couresName: "Learn Data Stucture",
+        id:6,
+        img: "https://i.postimg.cc/ry9mJp7m/Rectangle-2-3.png",
+        price: 4250,
+        durution: 2,
+        desc:"We Provide best learing for the student. so you can sure that your children will be panko. thank you"
+    },
+]
